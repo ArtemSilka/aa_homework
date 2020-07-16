76eeaa24d67f298b6fb2d19a62e092e4752352ba
Artem Silka <silkartem@gmail.com>
2020-07-15 23:41:17 -0400

    Job Listing

---
+++ W10D4/job_listing/frontend/actions.js
@@ -0,0 +1,13 @@
+export const SWITCH_LOCATION = "SWITCH_LOCATION";
+
+export const selectLocation = (city, jobs) => {
+    return {
+        type: SWITCH_LOCATION,
+        city,
+        jobs,
+    };
+};
+
+export default selectLocation;
+
+window.selectLocation = selectLocation;
